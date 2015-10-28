755781883f62d7327ef521b6c3bf0e229fc066a1
Tiago Azevedo <tiago.azevedo@red-badger.com>
2015-10-28 11:43:16 +0000

    Router middleware tests, better assertion in render middleware

--- spec/unit/server/middleware/render.js
+++ spec/unit/server/middleware/render.js
@@ -22,13 +22,14 @@ describe('Render Middleware', () => {
 
         yield next;
       })
-      .use(render)
-      .use(function* (){
-        assert.match(this.body, /<html><body><script type=\"application\/json\" id=\"myapp-state\">(?:.+)<\/script><div(?:.*)id="myapp"(?:.*)>(?:.*)<\/div><\/body><\/html>/);
-      });
+      .use(render);
 
     request(http.createServer(app.callback()))
       .get('/')
-      .end(done);
+      .end((err, res) => {
+        if (err) throw err;
+        assert.match(res.text, /<html><body><script type=\"application\/json\" id=\"myapp-state\">(?:.+)<\/script><div(?:.*)id="myapp"(?:.*)>(?:.*)<\/div><\/body><\/html>/)
+        done();
+      });
   });
 });
